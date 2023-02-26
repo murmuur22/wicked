6424e80abafed580858bfc2ec3f4da9909ca9576
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2023-02-26 18:44:30 -0500

    refactored loading screen

--- src/components/VerifyingClient.jsx
+++ src/components/VerifyingClient.jsx
@@ -5,13 +5,8 @@ function LoginItem ({alt, imgRef}){
 
     return(
     <div className="fixed inset-0 flex items-center justify-center flex-col text-center">
-        <img 
-            src={logoRef}
-            alt='wicked logo'
-            className='w-24 md:w-48'
-        />
         <h1 className="text-2xl md:text-4xl pb-0 mb-0 font-display">verifying client...</h1>
-        <p className="text-sm mb-3 font-terminal">
+        <p className="fixed inset-x-0 bottom-0 text-sm mb-3 font-terminal">
             <span>{siteVersion}</span> <span>by robbie dyson</span>
         </p>
     </div>
