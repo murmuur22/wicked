3f7ab1d7c1b6af0cc57040b9d03115730a86fad3
murmuur22 <69942927+murmuur22@users.noreply.github.com>
2023-03-01 16:39:24 +0000

    Build: (705b8cb) changed from class to functional components, started adding a cursor trail component

---
+++ assets/CursorTrail-37933946.js
@@ -0,0 +1,1 @@
+import{r as o,j as l,F as a,a as e}from"./index-34acc950.js";function n(){const[t,i]=o.useState({x:0,y:0});return o.useEffect(()=>{window.onmousemove=s=>{i({x:s.clientX,y:s.clientY})}}),console.log(t),l(a,{children:[e("div",{className:"w-full h-full absolute bg-yellow-400",id:"bkg"}),e("div",{id:"trailer"})]})}export{n as default};
