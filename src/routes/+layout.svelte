6eb85507cc74e8138ea461e33c9f245cda0bf241
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2023-12-18 04:26:20 -0500

    added vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 
-    /* Add Vercel Analytics */
+    /* Add Vercel analytics */
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
+
+    /* Add Vercel speed insights */
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+    injectSpeedInsights()
     
     inject({ mode: dev ? 'development' : 'production' });
 
