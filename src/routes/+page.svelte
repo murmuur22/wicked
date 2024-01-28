59433f30a1a26bdeef5c991098d17ca087f5315e
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-01-28 12:51:33 -0500

    deploying

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
     
     export let data;
     let { subdirs, files } = data;
-
+    // bla
     let windowRefs = files.map(() => ""); // Create list of window references at same length of MAPS
 </script>
 
